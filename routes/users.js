995871e9084e7be012a90a0ee3fffa88b9dcc42f
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-04-11 04:53:43 +0500

    user model and routes update

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,38 @@
 const router=require("express").Router();
+const User=require('../models/User')
 
 
+
+router.put("/:id",async(req,res)=>{
+	if(req.body.userId==req.params.id || req.body.isAdmin){
+		if(req.body.password){
+			try{
+				const salt=await bcrypt.genSalt(10);
+				req.body.password=await bcrypt.hash(req.body.password,salt);
+
+
+			}
+			catch(err){
+				console.error(err)
+				return res.status(500).json("You can update only your account!")
+			}
+		}
+		try{
+			const user=await User.findByIdAndUpdate(req.params.id,{
+				$set:req.body
+			})
+			res.status(200).json("Account updated")
+
+		}
+		catch(err){
+			console.error(err)
+			return res.status(500).json("You can update only your account!")
+		}
+	}
+	else{
+		res.status(403).json("You can only update your account!")
+	}
+})
 
 router.get("/",(req,res)=>{
 	res.send("user route")
